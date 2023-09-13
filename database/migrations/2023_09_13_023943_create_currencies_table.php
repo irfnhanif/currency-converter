11a3543583f3f7bc21e016dd8142afe490c0cb07
Irfan Hanif <14.irfanh@gmail.com>
2023-09-13 11:04:14 +0700

    created user, currency, and rate models and migrated them

--- database/migrations/2023_09_13_023943_create_currencies_table.php
+++ database/migrations/2023_09_13_023943_create_currencies_table.php
@@ -11,23 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('currencies', function (Blueprint $table) {
             $table->id();
+            $table->string('currency_code');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->string('default_currency');
-            $table->timestamps();
+            $table->string('symbol');
         });
     }
-
+    
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('currencies');
     }
 };
--- database/migrations/2023_09_13_025507_create_rates_table.php
+++ database/migrations/2023_09_13_025507_create_rates_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rates', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->string('default_currency');
+            $table->integer('currency_from_id');
+            $table->integer('currency_to_id');
+            $table->decimal('rate', 17, 8);
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rates');
     }
 };
