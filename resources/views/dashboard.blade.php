20427f7d95f1e1eada74d08f7edc429c63a06490
Irfan Hanif <14.irfanh@gmail.com>
2023-09-23 11:33:07 +0700

    added necessary input and select elements for UI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,39 +7,55 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h1 class="text-4xl pb-5">Convert Currency</h1>
-                    <div class="flex justify-evenly pb-16">
+                    <div class="flex justify-evenly">
                         <div class="form-control w-full max-w-sm p-4">
-                        <label class="label">
-                            <span class="label-text text-lg">Amount</span>
-                        </label>
-                        <input type="text" placeholder="Type here" class="input input-bordered rounded-box w-full max-w-xs" />
+                            <label class="label">
+                                <span class="label-text text-lg">Amount</span>
+                            </label>
+                            <input id="selected-symbol-input" type="text" value="" class="input input-bordered rounded-box w-full max-w-xs" />
                         </div>
 
-                        <div class="form-control w-full max-w-sm dropdown dropdown-end p-4">
-                        <label class="label">
-                            <span class="label-text text-lg">From</span>
-                        </label>
-                        <label tabindex="0" class="btn m-1">Click</label>
-                        <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                            @foreach ($currencies as $currency)
-                                <li><a><span class="uppercase">{{ $currency['currency_code'] }} </span>- {{ $currency['name'] }}</a></li>
-                            @endforeach
-                        </ul>
+                        <div class="form-control w-full max-w-sm p-4">
+                            <label class="label">
+                                <span class="label-text text-lg">From</span>
+                            </label>
+                            <select id="currency-select" class="select select-bordered">
+                                @foreach ($currencies as $currency)
+                                <option value="{{ $currency['symbol'] }}"><span class="uppercase">{{ $currency['currency_code'] }}</span> - {{ $currency['name'] }}</option>
+                                @endforeach
+                            </select>
                         </div>
 
-                        <div class="form-control w-full max-w-sm dropdown dropdown-end p-4">
-                        <label class="label">
-                            <span class="label-text text-lg">To</span>
-                        </label>
-                        <label tabindex="0" class="btn m-1">Click</label>
-                        <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 overflow-y-auto">
-                            @foreach ($currencies as $currency)
-                                <li><a><span class="uppercase">{{ $currency['currency_code'] }} </span>- {{ $currency['name'] }}</a></li>
-                            @endforeach
-                        </ul>
+                        <script>
+                            const selectedCurrencyInput = document.getElementById('selected-symbol-input');
+                            const currencySelect = document.getElementById('currency-select');
+                            let currencySymbol;
+
+                            currencySelect.addEventListener('change', (event) => {
+                                selectedCurrencyInput.value = event.target.value;
+                                currencySymbol = event.target.value;
+                            });
+
+                            selectedCurrencyInput.addEventListener('input', (event) => {
+                                if (!event.target.value.startsWith(currencySymbol)) {
+                                    event.target.value = currencySymbol + event.target.value;
+                                }
+                            });
+                        </script>
+
+                        <div class="form-control w-full max-w-sm p-4">
+                            <label class="label">
+                                <span class="label-text text-lg">To</span>
+                            </label>
+                            <select class="select select-bordered">
+                                @foreach ($currencies as $currency)
+                                <option><span class="uppercase">{{ $currency['currency_code'] }}</span> - {{ $currency['name'] }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                         </div>
                     </div>
                 </div>
