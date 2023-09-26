0d5a76353e569fe39c1f0234c3087c03c6afb52f
Irfan Hanif <14.irfanh@gmail.com>
2023-09-26 13:31:19 +0700

    added convert button and fix some bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,63 +10,68 @@
             <div class="bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h1 class="text-4xl pb-5">Convert Currency</h1>
-                    <div class="flex justify-evenly">
-                        <div class="form-control w-full max-w-sm p-4">
-                            <label class="label">
-                                <span class="label-text text-lg">Amount</span>
-                            </label>
-                            <input id="selected-symbol-input" type="text" value="" class="input input-bordered rounded-box w-full max-w-xs" />
+                    <form action="" method="post">
+                        <div class="flex justify-evenly">
+                            <div class="form-control w-full max-w-sm p-4">
+                                <label class="label">
+                                    <span class="label-text text-lg">Amount</span>
+                                </label>
+                                <input id="selected-symbol-input" type="text" value="$" class="input input-bordered rounded-box w-full max-w-xs" />
+                            </div>
+                            <div class="form-control w-full max-w-sm p-4">
+                                <label class="label">
+                                    <span class="label-text text-lg">From</span>
+                                </label>
+                                <select id="currency-select" class="select select-bordered">
+                                    @foreach ($currencies as $currency)
+                                    <option value="{{ $currency['symbol'] }}"><span class="uppercase">{{ $currency['currency_code'] }}</span> - {{ $currency['name'] }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div class="form-control w-full max-w-sm p-4">
+                                <label class="label">
+                                    <span class="label-text text-lg">To</span>
+                                </label>
+                                <select class="select select-bordered">
+                                    @foreach ($currencies as $currency)
+                                    <option><span class="uppercase">{{ $currency['currency_code'] }}</span> - {{ $currency['name'] }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
-
-                        <div class="form-control w-full max-w-sm p-4">
-                            <label class="label">
-                                <span class="label-text text-lg">From</span>
-                            </label>
-                            <select id="currency-select" class="select select-bordered">
-                                @foreach ($currencies as $currency)
-                                <option value="{{ $currency['symbol'] }}"><span class="uppercase">{{ $currency['currency_code'] }}</span> - {{ $currency['name'] }}</option>
-                                @endforeach
-                            </select>
+                        <div class="flex justify-center py-5">
+                            <input type="submit" class="btn btn-active btn-neutral" value="Convert"/>
                         </div>
-
-                        <script>
-                            const selectedCurrencyInput = document.getElementById('selected-symbol-input');
-                            const currencySelect = document.getElementById('currency-select');
-                            let currencySymbol;
-
-                            currencySelect.addEventListener('change', (event) => {
-                                selectedCurrencyInput.value = event.target.value;
-                                currencySymbol = event.target.value;
-                            });
-
-                            selectedCurrencyInput.addEventListener('input', (event) => {
-                                const inputValue = event.target.value;
-                                const symbolLength = currencySymbol.length;
-
-                                if (!inputValue.startsWith(currencySymbol)) {
-                                    if (inputValue.length === symbolLength + 1) {
-                                        event.target.value = currencySymbol;
-                                    } else {
-                                        event.target.value = currencySymbol + inputValue.slice(symbolLength);
-                                    }
-                                }
-                            });
-                        </script>
-
-                        <div class="form-control w-full max-w-sm p-4">
-                            <label class="label">
-                                <span class="label-text text-lg">To</span>
-                            </label>
-                            <select class="select select-bordered">
-                                @foreach ($currencies as $currency)
-                                <option><span class="uppercase">{{ $currency['currency_code'] }}</span> - {{ $currency['name'] }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        const selectedCurrencyInput = document.getElementById('selected-symbol-input');
+        const currencySelect = document.getElementById('currency-select');
+        let currencySymbol;
+
+        selectedCurrencyInput.value = currencySelect.value;
+
+        currencySelect.addEventListener('change', (event) => {
+            selectedCurrencyInput.value = event.target.value;
+            currencySymbol = event.target.value;
+        });
+
+        selectedCurrencyInput.addEventListener('input', (event) => {
+            const inputValue = event.target.value;
+            const symbolLength = currencySymbol.length;
+
+            if (!inputValue.startsWith(currencySymbol)) {
+                if (inputValue.length === symbolLength + 1) {
+                    event.target.value = currencySymbol;
+                } else {
+                    event.target.value = currencySymbol + inputValue.slice(symbolLength);
+                }
+            }
+        });
+    </script>
 </x-app-layout>
