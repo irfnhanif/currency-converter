41907e500c947834411beeea5c8b6ead083ad61c
Irfan Hanif <14.irfanh@gmail.com>
2023-10-16 09:33:34 +0700

    updated result page

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -9,8 +9,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <p>{{ $amount }} {{ $fromCurrency['name'] }} = {{ $toCurrencyAmount }} {{ $toCurrency['name'] }}</p>
-                    <p>{{ $fromCurrency['symbol'] }}1 (<span class="uppercase">{{ $fromCurrency['currency_code'] }}</span>) = {{ $toCurrency['symbol'] }}{{ $oneAmountToCurrency }} (<span class="uppercase">{{ $toCurrency['currency_code'] }}</span>)</p>
+                    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white flex justify-center p-5">Convert Currency</h1>
+
+                    <p class="text-4xl font-medium text-gray-900 flex justify-center">{{ $amount }} {{ $fromCurrency['name'] }} = {{ $toCurrencyAmount }} {{ $toCurrency['name'] }}</p>
+
+                    <p class="text-lg text-gray-600 flex justify-center pt-3" >{{ $toCurrency['symbol'] }}1 (<span class="uppercase">{{ $toCurrency['currency_code'] }}</span>) = {{ $fromCurrency['symbol'] }}{{ $oneAmountToCurrency }} (<span class="uppercase">{{ $fromCurrency['currency_code'] }}</span>)</p>
+
+                    <p class="text-lg text-gray-600 flex justify-center pb-3">{{ $fromCurrency['symbol'] }}1 (<span class="uppercase">{{ $fromCurrency['currency_code'] }}</span>) = {{ $toCurrency['symbol'] }}{{ $oneAmountFromCurrency }} (<span class="uppercase">{{ $toCurrency['currency_code'] }}</span>)</p>
+
+                    <div class="flex justify-center pt-3 pb-2">
+                        <button class="btn btn-neutral">Return</button>
+                    </div>
                 </div>
             </div>
         </div>
